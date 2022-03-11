82645029876de0c20e2f4879e1bfb7e5c9046f54
Marine <marine.huygens@hotmail.fr>
2022-03-11 16:05:53 +0100

    last maj V2

--- scripts/index.js
+++ scripts/index.js
@@ -3,13 +3,16 @@ import RecipeDisplay from "./recipesDisplay.js";
 import createListElements from "./listDisplay.js";
 import tagDisplay from "./tagDisplay.js";
 import filterTagsbyInputTag from "./searchItem.js";
-import globalSearch from "./search.js";
+import eventSearch from "./eventSearch.js";
+import RecipesClean from "./CleanData/formattingBuilder.js";
 
 function buildDom() {
   RecipeDisplay.createCardRecipe(recipes);
   createListElements();
   tagDisplay.createBoxTags();
   filterTagsbyInputTag();
+  eventSearch();
 }
 
 buildDom();
+export let recipesCleaned = RecipesClean.clean(recipes);
--- scripts/search.js
+++ scripts/search.js
@@ -3,124 +3,356 @@ import { listTag } from "./listDisplay.js";
 import RecipeDisplay from "./recipesDisplay.js";
 import filterTagsbyInputTag from "./searchItem.js";
 import tagDisplay from "./tagDisplay.js";
-import { RecipesClean } from "../scripts/CleanData/formattingBuilder.js";
+import { recipesCleaned } from "./index.js";
 
-export default function globalSearch() {
-  document.getElementById("search").addEventListener("input", (e) => {
-    let searchInLowerCase = document.getElementById("search").value.toLowerCase();
-    // all recipes card  if input value <= 2
-    if (e.target.value.length <= 2) {
-      const recipesCard = document.querySelectorAll(".article-recipes");
-      if (recipesCard.length) {
-        recipesCard.forEach((element) => {
-          element.remove();
-        });
-      }
-      // Display recipes
-      resultCardRecipes(recipes);
-      filterTagsbyInputTag();
-    }
-    // if input value >= 3
-    if (e.target.value.length >= 3) {
-      // remove all search items from their boxes
-      document.querySelectorAll(".search-item").forEach((item) => {
-        item.remove();
-      });
+let tagsSelected = [];
+let totalSelected = [];
+let inputValue = [];
+let results = [];
+let resultFinal = [];
+let resultRecipesMiniTags = [];
+let lastResultTotalMiniTags = [];
+let totalMiniTags = [];
+let listMiniTags;
+let resultsTag = [];
+let valueLowCase = [];
+let stockTag = [];
 
-      // search on cleaned recipes
-      let recipesCleaned = RecipesClean.clean(recipes);
-      // search (filter + includes) input value -> recipes (name, description, ingredients, ustensils, appliance)
+export default function search(dataValue, value) {
+  tagsSelected = document.querySelectorAll(".selected");
+  inputValue = document.getElementById("search").value.toLowerCase();
 
-      let results = recipesCleaned.filter((obj) => {
-        return (
-          obj.name.toLowerCase().includes(searchInLowerCase) ||
-          obj.description.toLowerCase().includes(searchInLowerCase) ||
-          obj.ingredientsString.toLowerCase().includes(searchInLowerCase)
-        );
-      });
-
-      let finalResult = [];
-      results.forEach((res) => {
-        let id = res.id;
-        res = !id ? recipes : recipes.filter((el) => el.id == id);
-        finalResult.push(...res);
-        return finalResult;
-      });
-
-      results = finalResult;
-
-      resultCardRecipes(results);
-      tagDisplay.createBoxTags();
-      tagFromSearch(results);
-      filterTagsbyInputTag();
-    }
-  });
-  // remove all cards
-  document.querySelectorAll(".article-recipes").forEach((item) => {
-    item.remove();
-  });
-  function resultCardRecipes(results) {
-    // display none/block if results or not
-    let MsgNoResult = document.querySelector("#no-results");
-    if (results.length === 0) {
-      MsgNoResult.style.display = "block";
-    } else {
-      MsgNoResult.style.display = "none";
-    }
-    RecipeDisplay.createCardRecipe(results);
+  if ((tagsSelected.length == 0 && inputValue.length == 0) || (tagsSelected.length == 0 && inputValue.length <= 2)) {
+    results = [];
+    results = recipes;
+    UpdateItemsList(results);
+    resultCardRecipes(results);
   }
+
+  if (tagsSelected.length == 1 && inputValue.length == 0) {
+    deleteItem();
+
+    tagsSelected = document.querySelectorAll(".selected");
+    tagsSelected.forEach((tagsSelected) => {
+      totalSelected.push(tagsSelected.innerText);
+
+      for (let i = 0; i < totalSelected.length; i++) {
+        totalMiniTags.push(
+          ...[
+            {
+              datavalue: dataValue,
+            },
+          ]
+        );
+      }
+    });
+
+    totalMiniTags = uniqueList(totalMiniTags, "datavalue");
+    stockTag = uniqueList(totalMiniTags, "datavalue");
+    lastResultTotalMiniTags.push(...totalMiniTags);
+    valueLowCase = value;
+    stockTag.push({ value: valueLowCase });
+
+    if (totalSelected.length > 0) {
+      switch (dataValue) {
+        case "ingredients":
+          resultRecipesMiniTags = recipes.filter((obj) => obj.ingredients.find((ingredient) => ingredient.ingredient.toLowerCase() === valueLowCase));
+          resultFinal.push(...resultRecipesMiniTags);
+          break;
+
+        case "appliance":
+          resultRecipesMiniTags = recipes.filter((obj) => obj.appliance.toLowerCase() === valueLowCase);
+          resultFinal.push(...resultRecipesMiniTags);
+          break;
+
+        case "ustensils":
+          resultRecipesMiniTags = recipes.filter((obj) => obj.ustensils.find((ustensil) => ustensil.toLowerCase() === valueLowCase));
+          resultFinal.push(...resultRecipesMiniTags);
+          break;
+      }
+
+      results = [...new Set(resultFinal)];
+    }
+    UpdateItemsList(results);
+    resultCardRecipes(results);
+  }
+
+  if (tagsSelected.length >= 2 && inputValue.length == 0) {
+    deleteItem();
+    totalSelected = [];
+    tagsSelected = [];
+
+    tagsSelected = document.querySelectorAll(".selected");
+
+    tagsSelected.forEach((tagsSelected) => {
+      totalSelected.push(tagsSelected.innerText);
+      for (let i = 0; i < totalSelected.length; i++) {
+        totalMiniTags.push(
+          ...[
+            {
+              datavalue: dataValue,
+            },
+          ]
+        );
+      }
+    });
+
+    totalMiniTags = uniqueList(totalMiniTags, "datavalue");
+    valueLowCase = value;
+
+    resultRecipesMiniTags = results.filter(
+      (obj) =>
+        obj.ingredients.find((ingredient) => ingredient.ingredient.toLowerCase() === valueLowCase) ||
+        obj.appliance.toLowerCase() === valueLowCase ||
+        obj.ustensils.find((ustensil) => ustensil.toLowerCase() === valueLowCase)
+    );
+
+    results = [];
+    resultFinal = [];
+    resultFinal.push(...resultRecipesMiniTags);
+    results = [...new Set(resultFinal)];
+
+    resultCardRecipes(results);
+    UpdateItemsList(results);
+  }
+
+  if ((tagsSelected.length >= 1 && inputValue.length == 2) || (tagsSelected.length >= 1 && inputValue.length == 1)) {
+    totalSelected = [];
+    deleteItem();
+    stockTag.forEach((el) => {
+      totalSelected.push(el.value);
+      for (let i = 0; i < totalSelected.length; i++) {
+        totalMiniTags.push(
+          ...[
+            {
+              datavalue: dataValue,
+            },
+          ]
+        );
+      }
+    });
+
+    totalMiniTags = uniqueList(totalMiniTags, "datavalue");
+
+    valueLowCase = [];
+    stockTag.forEach((el) => {
+      valueLowCase = el.value;
+    });
+
+    resultRecipesMiniTags = recipes.filter(
+      (obj) =>
+        obj.ingredients.find((ingredient) => ingredient.ingredient.toLowerCase() === valueLowCase) ||
+        obj.appliance.toLowerCase() === valueLowCase ||
+        obj.ustensils.find((ustensil) => ustensil.toLowerCase() === valueLowCase)
+    );
+
+    resultFinal.push(...resultRecipesMiniTags);
+    results = [...new Set(resultFinal)];
+
+    UpdateItemsList(results);
+    resultCardRecipes(results);
+  }
+
+  if (tagsSelected.length == 0 && inputValue.length >= 3) {
+    deleteItem();
+
+    let results = recipesCleaned.filter((obj) => {
+      return (
+        obj.name.toLowerCase().includes(inputValue) ||
+        obj.description.toLowerCase().includes(inputValue) ||
+        obj.ingredientsString.toLowerCase().includes(inputValue)
+      );
+    });
+
+    let finalResult = [];
+    results.forEach((res) => {
+      let id = res.id;
+      res = !id ? recipes : recipes.filter((el) => el.id == id);
+      finalResult.push(...res);
+      return finalResult;
+    });
+
+    results = finalResult;
+
+    UpdateItemsList(results);
+    results = [...new Set(results)];
+    resultCardRecipes(results);
+  }
+
+  if (tagsSelected.length >= 1 && inputValue.length >= 3) {
+    resultFinal = [];
+    deleteItem();
+
+    let resultsInput = results.filter((obj) => {
+      return (
+        obj.name.toLowerCase().includes(inputValue) ||
+        obj.description.toLowerCase().includes(inputValue) ||
+        obj.ingredients.find((ingredient) => ingredient.ingredient.toLowerCase().includes(inputValue)) ||
+        obj.ustensils.find((ustensils) => ustensils.toLowerCase().includes(inputValue)) ||
+        obj.appliance.toLowerCase().includes(inputValue)
+      );
+    });
+
+    totalSelected = [];
+    tagsSelected = [];
+    tagsSelected = document.querySelectorAll(".selected");
+
+    tagsSelected.forEach((tagsSelected) => {
+      totalSelected.push(tagsSelected.innerText);
+      for (let i = 0; i < totalSelected.length; i++) {
+        totalMiniTags.push(
+          ...[
+            {
+              datavalue: dataValue,
+            },
+          ]
+        );
+      }
+    });
+
+    totalMiniTags = uniqueList(totalMiniTags, "datavalue");
+    lastResultTotalMiniTags.push(...totalMiniTags);
+    valueLowCase = value;
+
+    if (totalSelected.length > 0) {
+      switch (dataValue) {
+        case "ingredients":
+          resultRecipesMiniTags = resultsInput.filter((obj) =>
+            obj.ingredients.find((ingredient) => ingredient.ingredient.toLowerCase() === valueLowCase)
+          );
+          resultFinal.push(...resultRecipesMiniTags);
+          break;
+
+        case "appliance":
+          resultRecipesMiniTags = resultsInput.filter((obj) => obj.appliance.toLowerCase() === valueLowCase);
+          resultFinal.push(...resultRecipesMiniTags);
+          break;
+
+        case "ustensils":
+          resultRecipesMiniTags = resultsInput.filter((obj) => obj.ustensils.find((ustensil) => ustensil.toLowerCase() === valueLowCase));
+          resultFinal.push(...resultRecipesMiniTags);
+          break;
+      }
+
+      resultsTag = [...new Set(resultFinal)];
+    }
+
+    results = [];
+    results.push(...resultsTag);
+
+    UpdateItemsList(results);
+    results = [...new Set(results)];
+    resultCardRecipes(results);
+  }
+
+  tagDisplay.createBoxTags();
+  filterTagsbyInputTag();
 }
-// update tag from search (ingredients-appliances-ustensils)
-function tagFromSearch(results) {
-  // remove all items from their list
-  document.querySelectorAll(".search-item").forEach((item) => {
-    item.remove();
-  });
-  // ingredients [] from results
+
+function resultCardRecipes(results) {
+  // display none/block if results or not
+  let MsgNoResult = document.querySelector("#no-results");
+  if (results.length === 0) {
+    MsgNoResult.style.display = "block";
+  } else {
+    MsgNoResult.style.display = "none";
+  }
+  RecipeDisplay.createCardRecipe(results);
+}
+
+export function UpdateItemsList(results) {
+  deleteItem();
+
   let ingredientsResult = [];
   let reducedIngredient = [];
   results.forEach((recipe) => {
     if (recipe.ingredients.length) {
       const ingredientsMapResult = recipe.ingredients.map((ingr) => ingr.ingredient.toLowerCase());
       ingredientsResult.push(...ingredientsMapResult);
-      reducedIngredient = [...new Set(ingredientsResult)]; //remove duplicates
+      reducedIngredient = [...new Set(ingredientsResult)];
     }
   });
-  //   add new list ingredients
   reducedIngredient.forEach((element) => {
     document.querySelector(".search-list-ingredients").append(listTag(element, "ingredients"));
   });
-  // appliances [] from results
-  let appliancesResult = [];
-  let reducedAppliance = [];
+  let applianceResult = [];
+  let reducedappliance = [];
   results.forEach((recipe) => {
     if (recipe.appliance.length) {
-      const appliancesMapResult = recipe.appliance.toLowerCase();
-      appliancesResult.push(appliancesMapResult);
-      reducedAppliance = [...new Set(appliancesResult)]; //remove duplicates
+      const appliancesMapResult = recipe.appliance;
+      applianceResult.push(appliancesMapResult);
+      reducedappliance = [...new Set(applianceResult)];
     }
   });
-  // add new list appliance
-  reducedAppliance.forEach((element) => {
+  reducedappliance.forEach((element) => {
     document.querySelector(".search-list-appareils").append(listTag(element, "appliance"));
   });
-  // ustensils [] from results
+
   let ustensilsResult = [];
   let reducedUstensils = [];
   results.forEach((recipe) => {
     if (recipe.ustensils.length) {
-      recipe.ustensils.forEach((item) => {
-        ustensilsResult.push(item.toLowerCase());
-        reducedUstensils = [...new Set(ustensilsResult)]; //remove duplicates
-      });
+      const ustensilsMapResult = recipe.ustensils;
+      ustensilsResult.push(...ustensilsMapResult);
+      reducedUstensils = [...new Set(ustensilsResult)];
     }
   });
-  // add new list ustensils
   reducedUstensils.forEach((element) => {
     document.querySelector(".search-list-ustensils").append(listTag(element, "ustensils"));
   });
-  tagDisplay.createBoxTags();
 }
 
-const inputSearch = document.getElementById("search");
-globalSearch(inputSearch);
+export function updateAfterTagRemoved(dataValue) {
+  totalSelected = [];
+  tagsSelected = [];
+
+  deleteItem();
+
+  tagsSelected = document.querySelectorAll(".selected");
+  tagsSelected.forEach((tagsSelected) => {
+    let tags = tagsSelected.innerText.toLowerCase().split(" ");
+    tags.splice(1);
+
+    totalSelected.push(tags);
+
+    for (let i = 0; i < totalSelected.length; i++) {
+      totalMiniTags.push(
+        ...[
+          {
+            datavalue: dataValue,
+          },
+        ]
+      );
+    }
+  });
+
+  totalMiniTags = uniqueList(totalMiniTags, "datavalue");
+
+  valueLowCase = totalSelected;
+
+  resultRecipesMiniTags = recipes.filter(
+    (obj) =>
+      obj.ingredients.find((ingredient) => ingredient.ingredient.toLowerCase().includes(valueLowCase)) ||
+      obj.appliance.toLowerCase() === valueLowCase ||
+      obj.ustensils.find((ustensil) => ustensil.toLowerCase() === valueLowCase)
+  );
+
+  resultFinal = [];
+  resultFinal.push(...resultRecipesMiniTags);
+
+  results = [];
+  results = [...new Set(resultFinal)];
+
+  resultCardRecipes(results);
+  UpdateItemsList(results);
+}
+
+function deleteItem() {
+  document.querySelectorAll(".search-item").forEach((item) => {
+    item.remove();
+  });
+}
+
+function uniqueList(arr, key) {
+  return [...new Map(arr.map((item) => [item[key], item])).values()];
+}
